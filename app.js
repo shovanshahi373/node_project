8d6358fc07167b55de05e41e10ab9c53adc1028b
sovan <sovanshahihero@gmail.com>
2019-05-20 09:21:07 +0545

    y

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const express = require("express");
+const path = require("path");
+const pug = require("pug");
 // const mongoose = require('mongoose');
 
 const connect = require("./database.js");
@@ -10,36 +12,44 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (request, response) => {
-    response.send("hello world!!!");
+  // response.send("hello world!!!");
+  response.render('hello_form');
 });
 
 app.get("/students", (request, response) => {
-    Student.find((err, studentList) => {
-        if (err) response.send({
-            error: err.message
-        });
-        response.send(studentList);
-    });
+  Student.find((err, studentList) => {
+    if (err)
+      response.send({
+        error: err.message
+      });
+    response.send(studentList);
+  });
 });
 
 app.post("/students", (request, response) => {
-    student = request.body;
-    Student.create(student, (err, createData) => {
-        if (err) response.send({
-            err: err.message
-        });
-        response.send(createData);
-    });
-    // console.log(request)
-    // response.send('created user')
+  student = request.body;
+  Student.create(student, (err, createData) => {
+    if (err)
+      response.send({
+        err: err.message
+      });
+    response.send(createData);
+  });
+
+
+  // console.log(request)
+  // response.send('created user')
 });
 
 function listner() {
-    app.listen(port, () => console.log(`server is running in port ${port}`));
+  app.listen(port, () => console.log(`server is running in port ${port}`));
 }
 
 connect()
-    .on("error", err => console.log("error occured" + err))
-    .once("connected", () => listner())
-    .on("disconnected", () => connect());
+  .on("error", err => console.log("error occured" + err))
+  .once("connected", () => listner())
+  .on("disconnected", () => connect());
